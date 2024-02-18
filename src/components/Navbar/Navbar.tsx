ed1a3cda5d24b75af45804ba859916c09e56ede0
kk9uk <122882463+kk9uk@users.noreply.github.com>
2024-02-18 20:52:03 +0800

    Created Navbar & Designed icon & name

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Flex, Image } from "@chakra-ui/react";
+
+const Navbar: React.FC = () => {
+    return (
+        <Flex bg="#1A83A8" height="44px" padding="6px 12px">
+            <Flex align="center">
+                <Image src="/images/icon.png" height="30px"/>
+                <Image src="/images/name.svg" height="18px" display={{ base: "none", md: "unset" }}/>
+            </Flex>
+        </Flex>
+    );
+};
+
+export default Navbar;
