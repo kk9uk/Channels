50feafa5724ba35b6098eeb0f34f086d390c359b
kk9uk <122882463+kk9uk@users.noreply.github.com>
2024-02-20 08:46:37 +0800

    Firebase Auth -> Client done

--- src/components/Navbar/Accountbar/Accountbar.tsx
+++ src/components/Navbar/Accountbar/Accountbar.tsx
@@ -1,21 +1,33 @@
 import React from "react";
-import { Flex, Button } from "@chakra-ui/react";
+import { Flex, Icon } from "@chakra-ui/react";
+import { GrAdd } from "react-icons/gr";
 
-import { signOut } from "firebase/auth";
-import { auth } from "../../../firebase/clientApp";
+import { User } from "firebase/auth";
 
 import Auth from "./Auth";
 import AuthPopup from "../../Popup/Auth/AuthPopup";
+import DropdownMenu from "./DropdownMenu";
 
 type AccountbarProp = {
-    user: any;
+    user?: User | null;
 }
 
 const Accountbar: React.FC<AccountbarProp> = ({ user }) => (
     <>
         <AuthPopup/>
         <Flex justify="center" align="center">
-            {user ? <Button onClick={() => signOut(auth)}>Logout</Button> : <Auth/>}
+            {user ? <Flex
+                        mr={1.5}
+                        ml={1.5}
+                        padding={1}
+                        cursor="pointer"
+                        borderRadius={4}
+                        _hover={{ bg: "blue.400" }}
+                    >
+                        <Icon as={GrAdd} fontSize={20} color="#FFFFFF"/>
+                    </Flex>
+                  : <Auth/>}
+            <DropdownMenu user={user}/>
         </Flex>
     </>
 );
