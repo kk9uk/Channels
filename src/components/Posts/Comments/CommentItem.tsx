6482068e8807c2f1d3f69845767dac75b6b17716
tch0905 <a1182116615@gmail.com>
2024-04-06 18:18:11 +0800

    comment and delete comment

--- src/components/Posts/Comments/CommentItem.tsx
+++ src/components/Posts/Comments/CommentItem.tsx
@@ -3,6 +3,7 @@ import { Timestamp } from 'firebase/firestore';
 import moment from 'moment';
 import React from 'react'
 import { FaUserCircle } from 'react-icons/fa';
+import {IoArrowDownCircleOutline, IoArrowUpCircleOutline} from "react-icons/io5";
 
 export type Comment = {
     id: string;
@@ -17,9 +18,9 @@ export type Comment = {
 
 type CommentItemProps = {
     comment: Comment;
-    onDeleteComment: (commentId: string) => void;
+    onDeleteComment: (commentId: Comment) => void;
     loadingDelete: boolean;
-    userId: 
+    userId?: string;
 };
 
 const CommentItem: React.FC<CommentItemProps> = ({
@@ -31,7 +32,7 @@ const CommentItem: React.FC<CommentItemProps> = ({
     return (
         <Flex>
             <Box mr={2}>
-                <Icon as={FaUserCircle} />
+                <Icon as={FaUserCircle} fontSize={30} color="gray.300"/>
             </Box>
             <Stack spacing={1}>
                 <Stack direction="row" align={"center"} fontSize={"8px"}>
@@ -42,10 +43,34 @@ const CommentItem: React.FC<CommentItemProps> = ({
                         </Text>
                     )}
                 </Stack>
-                <Box>
-                    {comment.text}
-                </Box>
+                <Text fontSize="10pt">{comment.text}</Text>
+                <Stack
+                    direction="row"
+                    align="center"
+                    cursor="pointer"
+                    fontWeight={600}
+                    color="gray.500"
+                >
+                    <Icon as={IoArrowUpCircleOutline} />
+                    <Icon as={IoArrowDownCircleOutline} />
+                    {userId === comment.creatorId && (
+                        <>
+                            <Text fontSize="9pt" _hover={{ color: "blue.500" }}>
+                                Edit
+                            </Text>
+                            <Text
+                                fontSize="9pt"
+                                _hover={{ color: "blue.500" }}
+                                onClick={() => onDeleteComment(comment)}
+                            >
+                                Delete
+                            </Text>
+                        </>
+                    )}
+                </Stack>
             </Stack>
+
+
         </Flex>
     )
 }
--- src/components/Posts/Comments/Comments.tsx
+++ src/components/Posts/Comments/Comments.tsx
@@ -1,19 +1,28 @@
 import React, { useEffect, useState } from "react";
 import { Post, postState } from "../../../state/postState";
 import { User } from "firebase/auth";
-import { Box, Flex, Stack } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Skeleton,
+  SkeletonCircle,
+  SkeletonText,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
 import CommentInput from "./CommentInput";
 import {
   collection,
-  doc,
-  increment,
+  doc, getDocs,
+  increment, orderBy, query,
   serverTimestamp,
-  Timestamp,
+  Timestamp, where,
   writeBatch,
 } from "firebase/firestore";
 import { firestore } from "../../../firebase/clientApp";
 import CommentItem, { Comment } from "./CommentItem";
 import { useSetRecoilState } from "recoil";
+
 
 type CommentsProps = {
   user: User;
@@ -22,14 +31,15 @@ type CommentsProps = {
 };
 
 const Comments: React.FC<CommentsProps> = ({
-  user,
-  selectPost,
-  channelName,
-}) => {
+                                             user,
+                                             selectPost,
+                                             channelName,
+                                           }) => {
   const [commentText, setCommentText] = useState("");
   const [comments, setComments] = useState<Comment[]>([]);
-  const [fetchLoading, setFetchLoading] = useState(false);
+  const [fetchLoading, setFetchLoading] = useState(true);
   const [createLoading, setCreateLoading] = useState(false);
+  const [loadingDeleteId, setLoadingDeleteId] = useState('');
   const setPostStateVal = useSetRecoilState(postState);
 
   const onCreateComment = async (commentText: string) => {
@@ -52,6 +62,8 @@ const Comments: React.FC<CommentsProps> = ({
 
       batch.set(commentDocRef, newComment);
 
+      newComment.createdAt = { seconds: Date.now() / 1000 } as Timestamp;
+
       const postDocRef = doc(firestore, "posts", selectPost?.id!);
       batch.update(postDocRef, {
         numberOfComments: increment(1),
@@ -61,60 +73,139 @@ const Comments: React.FC<CommentsProps> = ({
 
       setCommentText("");
       setComments((prev) => [newComment, ...prev]);
+      //TODO: the num of comment is not working
       setPostStateVal((prev) => ({
         ...prev,
         selectedPost: {
-          ...prev.selectedPost,
-          numberOfComments: prev.selectedPost?.numComments! + 1,
-        } as Post,
+          ...prev.selectedPost!,
+          numberOfComments: prev.selectedPost!.numComments! + 1,
+        },
       }));
     } catch (error) {
       console.log("onCreateComment error", error);
     }
     setCreateLoading(false);
+  };
 
-    // update client recoil state
-  };
-  const onDeleteComment = async (comment: string) => {
+  const onDeleteComment = async (comment: Comment) => {
     // create a comment doc
-    // update post numOfComments -11
+    // update post numOfComments -1
     // update client recoil state
+    setLoadingDeleteId(comment.id);
+    try {
+      // if (!comment.id) throw "Comment has no ID";
+      const batch = writeBatch(firestore);
+      const commentDocRef = doc(firestore, "comments", comment.id);
+      batch.delete(commentDocRef);
+
+      batch.update(doc(firestore, "posts", comment.postId), {
+        numberOfComments: increment(-1),
+      });
+
+      await batch.commit();
+
+      setPostStateVal((prev) => ({
+        ...prev,
+        selectedPost: {
+          ...prev.selectedPost,
+          numberOfComments: prev.selectedPost?.numComments! - 1,
+        } as Post,
+        postUpdateRequired: true,
+      }));
+
+      setComments((prev) => prev.filter((item) => item.id !== comment.id));
+    } catch (error: any) {
+      console.log("Error delete comment", error.message);
+
+    }
+    setLoadingDeleteId('');
   };
-  const getPostComments = async () => {};
+
+  const getPostComments = async () => {
+    try {
+      const commentsQuery = query(
+          collection(firestore, "comments"),
+          where("postId", "==", selectPost?.id),
+          orderBy("createdAt", "desc")
+      );
+      const commentDocs = await getDocs(commentsQuery);
+      const comments = commentDocs.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setComments(comments as Comment[]);
+    } catch (error: any) {
+      console.log("getPostComments error", error.message);
+    }
+    setFetchLoading(false);
+  };
 
   useEffect(() => {
+    if(!selectPost) return;
+    console.log("HERE IS SELECTED POST", selectPost?.id);
+
     getPostComments();
-  }, []);
+  }, [selectPost]);
+
   return (
-    <Box bg="white" borderRadius="0px 0px 4px 4px " p={2}>
-      <Flex
-        direction={"column"}
-        pl={10}
-        pr={4}
-        mb={6}
-        fontSize={"10pt"}
-        width={"100%"}
-      >
-        <CommentInput
-          commentText={commentText}
-          setCommentText={setCommentText}
-          user={user}
-          createLoading={createLoading}
-          onCreateComment={onCreateComment}
-        />
-      </Flex>
-      <Stack spacing={6} p={2}>
-        {comments.map((item: Comment) => (
-          <CommentItem
-            key={item.id}
-            comment={item}
-            onDeleteComment={onCreateComment}
-            loadingDelete={false}
-            userId={user.uid}
-        />
-        ))}
-      </Stack>
-    </Box>
+      <Box bg="white" borderRadius="0px 0px 4px 4px" p={2}>
+        <Flex
+            direction="column"
+            pl={10}
+            pr={4}
+            mb={6}
+            fontSize="10pt"
+            width="100%"
+        >
+          <CommentInput
+              commentText={commentText}
+              setCommentText={setCommentText}
+              user={user}
+              createLoading={createLoading}
+              onCreateComment={onCreateComment}
+          />
+        </Flex>
+        <Stack spacing={6} p={2}>
+          {fetchLoading ? (
+              <>
+                {[0, 1, 2].map((item) => (
+                    <Box key={item} padding="6" bg="white">
+                      <SkeletonCircle size="10" />
+                      <SkeletonText mt="4" noOfLines={2} spacing="4" />
+                    </Box>
+                ))}
+              </>
+          ) : (
+              <>
+                {comments.length === 0 ? (
+                    <Flex
+                        direction="column"
+                        justify="center"
+                        align="center"
+                        borderTop="1px solid"
+                        borderColor="gray.100"
+                        p={20}
+                    >
+                      <Text fontWeight={700} opacity={0.3}>
+                        No Comments Yet
+                      </Text>
+                    </Flex>
+                ) : (
+                    comments.map((item: Comment) => (
+                        <CommentItem
+                            key={item.id}
+                            comment={item}
+                            onDeleteComment={onDeleteComment}
+                            loadingDelete={loadingDeleteId === item.id}
+                            userId={user.uid}
+                        />
+                    ))
+                )}
+              </>
+          )}
+        </Stack>
+      </Box>
   );
 };
+
 export default Comments;
