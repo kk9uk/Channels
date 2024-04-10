7b09a1ea322de9c9f845717e38a518fde9f63577
tch0905 <a1182116615@gmail.com>
2024-04-10 17:19:57 +0800

    admin Page and admin UI design. Delete user function

---
+++ src/pages/admin/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ContentLayout from "../../components/Layout/ContentLayout";
+import AdminHeader from "../../components/Adminpage/adminHeader";
+const UserTable = () => {
+
+    return (
+        <ContentLayout>
+            <>
+                <AdminHeader></AdminHeader>
+            </>
+            <>
+            </>
+        </ContentLayout>
+
+    );
+};
+
+export default UserTable;
