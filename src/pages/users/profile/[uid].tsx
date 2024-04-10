15e949cd1d52b6f865aebd4050061c977d7af890
Reeves Lai <supernovareeves@gmail.com>
2024-04-10 23:02:27 +0800

    user Profile's Post

--- src/pages/users/profile/[uid].tsx
+++ src/pages/users/profile/[uid].tsx
@@ -1,9 +1,9 @@
-import React, { useEffect } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { firestore } from "../../../firebase/clientApp";
 import ContentLayout from "../../../components/Layout/ContentLayout";
 import UserHeader from "../../../components/UserProfile/UserHeader";
 import UserImageEditPopup from "../../../components/UserProfile/UserImageEditPopup";
-import { doc, getDoc } from "firebase/firestore";
+import { collection, doc, getDoc, getDocs, limit, orderBy, query, where } from "firebase/firestore";
 import { useRouter } from "next/router";
 import { getAuth } from "firebase/auth";
 import useUser from "../../../hooks/useUser";
@@ -11,14 +11,21 @@ import { Post } from "../../../state/postState";
 import { User, userState } from "../../../state/userState";
 import { useSetRecoilState } from "recoil";
 import UserNotFound from "../../../components/UserProfile/userNotfound";
+import { Stack, Text } from "@chakra-ui/react";
+import usePosts from "../../../hooks/usePosts";
+import PostItem from "../../../components/Posts/PostItem";
+import PostLoader from "../../../components/Posts/PostLoader";
 
 const UserPage: React.FC = () => {
     const router = useRouter();
+    const [loading, setLoading] = useState(false);
     const {
         setUserStateValue,
         userStateValue,
     } = useUser();
-
+    const { postStateVal, setPostStateVal, onPushPull, onSelect, onDelete, onTweet } = usePosts();
+    const currentUser = getAuth().currentUser?.uid;
+    const { uid } = router.query;
 
     const fetchUser = async (uid: string) => {
         try {
@@ -37,19 +44,41 @@ const UserPage: React.FC = () => {
         }
     };
 
+    const buildUserProfileFeed = useCallback(async () => {
+        setLoading(true);
+        try {
+            const postQuery = query(
+                collection(firestore, "posts"),
+                where("creatorId", "==", uid),
+                orderBy("createdAt", "desc"),
+                limit(6)
+            );
+            console.log(uid);
+            const postDocs = await getDocs(postQuery);
+            const post = postDocs.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+            console.log("Posts:", post)
+            setPostStateVal((prev) => ({
+              ...prev,
+              postList: post as Post[],
+            }));
+        } catch (error) {
+            console.log("buildNoUserHomeFeed error: ", error);
+        }
+        setLoading(false);
+    }, [setPostStateVal, router.query]);
+
     useEffect(() => {
         // const current_user = getAuth();
         // console.log("current_user",current_user.currentUser?.uid)
-        const { uid } = router.query;
         console.log("query:", uid);
-
-        if (uid && ! userStateValue.selectedUser) {
+        if (uid && !userStateValue.selectedUser) {
             console.log("fetching is processing")
             fetchUser(uid as string);
+            buildUserProfileFeed();
         }
         console.log("selected User ", userStateValue.selectedUser);
 
-    }, [router.query, userStateValue.selectedUser]);
+    }, [router.query, userStateValue.selectedUser, buildUserProfileFeed, uid, fetchUser]);
 
     return (
         <ContentLayout>
@@ -58,7 +87,30 @@ const UserPage: React.FC = () => {
                 {userStateValue.selectedUser && <UserHeader user={userStateValue.selectedUser} />}
             </>
             <>
-                {/* Additional components or content */}
+            {loading ? (
+          <PostLoader />
+        ) : (
+          <Stack>
+            {postStateVal.postList.map((post: Post) => (
+              <PostItem
+                key={post.id}
+                post={post}
+                isCreator={false}
+                numPushPull={post.numPushPull}
+                onPushPull={onPushPull}
+                onDelete={onDelete}
+                onSelect={onSelect}
+                onTweet={onTweet}
+                userPushPostValue={
+                  postStateVal.postPushPulls.find(
+                    (item) => item.postId === post.id
+                  )?.pushPullValue
+                }
+              />
+            ))}
+          </Stack>
+        )}
+
             </>
         </ContentLayout>
     );
